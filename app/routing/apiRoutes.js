7c976d644ad4b11c8d36dbf37da688d2b8eb079a
Katie Vogel <kvog983@gmail.com>
2019-07-28 15:25:25 -0400

    errors

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,49 +1,46 @@
-var userData = require("../data/friends");
-var newUserData = require("../data/new-friend")
+var usersArray = require("../data/friends");
+var newFriend = require("../data/new-friend")
 
-function empty() {
-  newFriend.length = 0;
-}
 module.exports = function(app) {
   
     app.get("/api/friends", function(req, res) {
-      res.json(userData);
+      res.json(usersArray);
+    
     });
 
     app.get("/api/new-friend", function(req, res) {
-      res.json(newUserData);
+      res.json(newFriend);
     });
    
     app.post("/api/friends", function(req, res) {
+      console.log(req.url);
+      console.log(req.body);
+      res.json(true);
       for (var i = 0; i < usersArray.length; i++) {
         if (newFriend[0] !== usersArray[i]) {
-          userData.push(req.body);
-          res.json(true);
-          empty();
+          usersArray.push(req.body);
         }
         else {
           console.log("Already entered a profile");
-          res.json(false);
-          empty();
         }
       }});
-    var newUser=req.body.scores;
-    for(var i = 0; i < newUserData.scores.length; i++) {
-      userData.scores[i] = parseInt(userData.scores[i]);
-    }
-    var gamerMatch = 0;
-    var minDiff = 40;
-    for(var i = 0; i < userData.length; i++) {
-      var totalDiff = 0;
-      for(var j = 0; j < userData[i].scores.length; j++) {
-        var scoreDiff = Math.abs(newUserData.scores[j] - userData[i].scores[j]);
-        totalDiff += scoreDiff;
-      }
-      if(totalDiff < minDiff) {
-        gamerMatch = i;
-        minDiff = totalDiff;
-      }
-    }
-    userData.push(newUserData);
-    res.json(userData[gamerMatch]);
+  //   var newUser=req.body.scores;
+  //   for(var i = 0; i < newFriend.scores.length; i++) {
+  //     usersArray.scores[i] = parseInt(usersArray.scores[i]);
+  //   }
+  //   var gamerMatch = 0;
+  //   var minDiff = 40;
+  //   for(var i = 0; i < usersArray.length; i++) {
+  //     var totalDiff = 0;
+  //     for(var j = 0; j < usersArray[i].scores.length; j++) {
+  //       var scoreDiff = Math.abs(newFriend.scores[j] - usersArray[i].scores[j]);
+  //       totalDiff += scoreDiff;
+  //     }
+  //     if(totalDiff < minDiff) {
+  //       gamerMatch = i;
+  //       minDiff = totalDiff;
+  //     }
+  //   }
+  //   usersArray.push(newFriend);
+  //   res.json(usersArray[gamerMatch]);
   };
